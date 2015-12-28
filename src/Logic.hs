982106093e1caedac61818b409b21dac0542091a
Taran Lynn <taranlynn0@gmail.com>
2015-12-28 14:25:01 -0800

    Removed redundant Rights in speed wires.

--- src/Logic.hs
+++ src/Logic.hs
@@ -13,24 +13,24 @@ accumB f = loop
 
 
 horizSpeed :: Wire s () NC.Curses (Maybe NC.Event) Float
-horizSpeed = mkPure_ $ \ev -> case ev of
-  (Just (NC.EventCharacter 'a')) -> Right (-1)
-  (Just (NC.EventCharacter 'A')) -> Right (-1)
-  (Just (NC.EventCharacter 'd')) -> Right 1
-  (Just (NC.EventCharacter 'D')) -> Right 1
-  _ -> Right 0
+horizSpeed = mkSF_ $ \ev -> case ev of
+  (Just (NC.EventCharacter 'a')) -> (-1)
+  (Just (NC.EventCharacter 'A')) -> (-1)
+  (Just (NC.EventCharacter 'd')) -> 1
+  (Just (NC.EventCharacter 'D')) -> 1
+  _ -> 0
 
 horizPos :: (HasTime t s) => Integer -> Wire s () NC.Curses (Maybe NC.Event) Integer
 horizPos width = fmap ((`mod` width) . floor) $ accumB (+) 0 . horizSpeed
 
 
 vertSpeed :: Wire s () NC.Curses (Maybe NC.Event) Float
-vertSpeed = mkPure_ $ \ev -> case ev of
-  (Just (NC.EventCharacter 'w')) -> Right (-1)
-  (Just (NC.EventCharacter 'W')) -> Right (-1)
-  (Just (NC.EventCharacter 's')) -> Right 1
-  (Just (NC.EventCharacter 'S')) -> Right 1
-  _ -> Right 0
+vertSpeed = mkSF_ $ \ev -> case ev of
+  (Just (NC.EventCharacter 'w')) -> (-1)
+  (Just (NC.EventCharacter 'W')) -> (-1)
+  (Just (NC.EventCharacter 's')) -> 1
+  (Just (NC.EventCharacter 'S')) -> 1
+  _ -> 0
 
 vertPos :: (HasTime t s) => Integer -> Wire s () NC.Curses (Maybe NC.Event) Integer
 vertPos height = fmap ((`mod` height) . floor) $ accumB (+) 0 . vertSpeed
